99245366d555c3d60b5f3752243331b21212fd18
danielrpg <daniel.fernandezgp@gmail.com>
2013-08-20 10:42:37 -0400

    modificaciond el model

---
+++ app/model/IndexModel.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once 'app/core/Mysql.php';
+/**
+ * Description of HomeModel
+ *
+ * @author Daniel Fernandez
+ */
+class IndexModel{
+    
+    /*
+     * Este es el constructo de del modelo HomeModel
+     */
+    public function __construct() {
+        
+    }
+    
+   
+    
+}
+
+?>
